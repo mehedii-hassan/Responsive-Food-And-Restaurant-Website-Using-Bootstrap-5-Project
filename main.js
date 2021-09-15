479805b3b2c72037589cb98ee161a0ac2d3e4a9c
mehediihassan <73456131+mehediihassan@users.noreply.github.com>
2021-09-14 21:41:30 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,23 +1,3 @@
-// window.onscroll = function() {myFunction()};
-
-// function myFunction() {
-//   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-//     document.getElementsByTagName("nav").className = "nav-style-change";
-//   } else {
-//     document.getElementsByClassName("nav").className = "";
-//   }
-// }
-
-// Scrollbar navbar 
-// let navBar = document.querySelector("#navMenu");
-// window.onscroll = function (){
-//     if(document.documentElement.scrollTop > 20 || document.body.scrollTop > 20){
-//         navBar.classList.add("scroll-on");
-//     }else{
-//         navBar.classList.remove('fixed-top');
-//     }
-// }
-
 let nav = document.querySelector("nav");
 window.onscroll = function (){
     if(document.documentElement.scrollTop > 20){
@@ -27,11 +7,4 @@ window.onscroll = function (){
     }
 }
 
-// nav hide  
-let navBar = document.querySelectorAll(".nav-link");
-let navCollapse = document.querySelector(".navbar-collapse.collapse");
-navBar.forEach(function (a){
-    a.addEventListener("click", function(){
-        navCollapse.classList.remove("show");
-    })
-})
+ 
