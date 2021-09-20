53ea94484f5757b3bb718f4f26f193bc83b5b305
mehediihassan <73456131+mehediihassan@users.noreply.github.com>
2021-09-20 08:14:06 -0700

    Back to Top button js added

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//Navbar effect after scrolling
 let nav = document.querySelector("nav");
 window.onscroll = function (){
     if(document.documentElement.scrollTop > 20){
@@ -5,6 +6,16 @@ window.onscroll = function (){
     }else{
         nav.classList.remove("background");
     }
-}
+};
+
+//Back to top button visible after scrolling
+const toTop = document.querySelector(".to-top");
+window.addEventListener('scroll',() => {
+    if(document.documentElement.scrollTop > 100){
+        toTop.classList.add('active');
+    }else{
+        toTop.classList.remove('active');
+    }
+});
 
  
